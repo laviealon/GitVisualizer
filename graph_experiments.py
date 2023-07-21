730a6190feac035e8f4bfa5075a020a7b07c3678
alonlavie <alon.lavie55@gmail.com>
2023-07-20 23:59:39 -0400

    Added drawing

--- graph_experiments.py
+++ graph_experiments.py
@@ -2,3 +2,6 @@ import networkx as nx
 
 if __name__ == '__main__':
     G = nx.DiGraph()
+    G.add_edge('a', 'b', weight=0.6)
+    nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
+    print(G.nodes())
